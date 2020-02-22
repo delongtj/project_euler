d7ce94d87276ac9ba74412148d2c8ca101a67e0d
Ty DeLong <delongtj@gmail.com>
2020-02-22 14:49:30 -0600

    Initial commit with tests

---
+++ lib/fibonacci_sequence.rb
@@ -0,0 +1,32 @@
+class FibonacciSequence
+
+  # Note: Performance of recursion breaks down quickly, so storing the sequence as an Array is more efficient at scale
+  attr_reader :numbers
+
+  def initialize(opts = { maximum: 4_000_000 })
+    maximum = opts[:maximum].to_i
+
+    raise "Invalid maximum" if maximum <= 0
+
+    # Sequence required to start at 1
+    @numbers = [ 1 ]
+
+    i = 2
+
+    while i <= maximum
+      @numbers << i
+
+      i = @numbers[ @numbers.size - 1 ] + @numbers[ @numbers.size - 2 ]
+    end
+  end
+
+  def sum(opts = { })
+    if opts[:parity].to_s == 'odd'
+      @numbers.select { |n| n % 2 != 0 }.sum
+    elsif opts[:parity].to_s == 'even'
+      @numbers.select { |n| n % 2 == 0 }.sum
+    else
+      @numbers.sum
+    end
+  end
+end
---
+++ test/test_fibonacci_sequence.rb
@@ -0,0 +1,49 @@
+require "minitest/autorun"
+require_relative "../lib/fibonacci_sequence"
+
+class TestFibonacciSequence < Minitest::Test
+
+  def test_that_constructor_requires_a_positive_integer
+    assert_raises "Invalid maximum" do
+      @fibonacci_sequence = FibonacciSequence.new(maximum: -1)
+    end
+
+    assert_raises "Invalid maximum" do
+      @fibonacci_sequence = FibonacciSequence.new(maximum: 0)
+    end
+  end
+
+  def test_that_maximum_is_enforced
+    @fibonacci_sequence = FibonacciSequence.new(maximum: 10)
+
+    # [ 1, 2, 3, 5, 8 ]
+    assert_equal 8, @fibonacci_sequence.numbers.last
+  end
+
+  def test_that_default_maximum_is_4_000_000
+    @fibonacci_sequence = FibonacciSequence.new()
+
+    assert_equal 3524578, @fibonacci_sequence.numbers.last
+  end
+
+  def test_that_sum_inludes_all_numbers_by_default
+    @fibonacci_sequence = FibonacciSequence.new(maximum: 100)
+
+    # [ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ]
+    assert_equal 231, @fibonacci_sequence.sum
+  end
+
+  def test_that_sum_can_include_only_even_numbers
+    @fibonacci_sequence = FibonacciSequence.new(maximum: 100)
+
+    # [ 2, 8, 34 ]
+    assert_equal 44, @fibonacci_sequence.sum(parity: 'even')
+  end
+
+  def test_that_sum_can_include_only_odd_numbers
+    @fibonacci_sequence = FibonacciSequence.new(maximum: 100)
+
+    # [ 1, 3, 5, 13, 21, 55, 89 ]
+    assert_equal 187, @fibonacci_sequence.sum(parity: 'odd')
+  end
+end
